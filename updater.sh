f44a89f4a3b21346c55670925de3b0878a890bff
Cpt <white.oil1433@fastmail.com>
2022-02-11 11:45:59 +0800

    test

--- updater.sh
+++ updater.sh
@@ -15,19 +15,20 @@ if [[ ${latest_version} = ${last_version} ]]; then
     exit
 fi
 
+# 生成SHA256SUM
 curl https://github.com/Nriver/trilium-translation/releases/download/v${latest_version}/trilium-cn-linux-x64.zip -LOC -
 sha256sum=$(sha256sum trilium-cn-linux-x64.zip | awk -F'  ' '{print $1}')
 
+# 更新缓存版本号
 mkdir workdir && cd workdir
-git clone https://${GITHUB_TOKEN}@${REPO}
-cd trilium-cn-aur-package
+git clone https://${REPO} && cd trilium-cn-aur-package
 echo ${latest_version} > LATEST
 git add LATEST
+git config user.name ${GH_NAME}
+git config user.email ${GH_EMAIL}
 git commit -m "New Version ${latest_version}"
-git push -u origin main
-
-cd ..
-git clone aur@aur.archlinux.org:trilium-bin-cn.git && cd trilium-bin-cn
+git push "https://${GITHUB_TOKEN}@${REPO}" main:main
+cd ../..
 
 # 编辑templete
 sed "s/%%pkgver%%/${latest_version}/g" PKGBUILD.templete -i
@@ -36,9 +37,18 @@ sed "s/%%sha256sum%%/${sha256sum}/g" PKGBUILD.templete -i
 sed "s/%%pkgver%%/${latest_version}/g" SRCINFO.templete -i
 sed "s/%%sha256sum%%/${sha256sum}/g" SRCINFO.templete -i
 
+# 更新AUR
+cd workdir
+git clone aur@aur.archlinux.org:trilium-bin-cn.git && cd trilium-bin-cn
+cp -f ../../PKGBUILD.templete ./PKGBUILD
+cp -f ../../SRCINFO.templete ./.SRCINFO
 git add PKGBUILD .SRCINFO
+git config user.name ${AUR_NAME}
+git config user.email ${AUR_EMAIL}
 git commit -m "Update ${latest_version}"
 git push -u origin master
 
 cd ../..
 echo -e "\e[32m ==========>\e[0m Package has been updated." >> message
+rm -rf workdir
+
